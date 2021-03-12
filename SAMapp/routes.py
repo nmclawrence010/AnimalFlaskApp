4b7bfc9afdde48085c3e1275b8111a50784612cc
Niall Mclawrence <you@example.comnmclawrence010@gmail.com>
2021-03-12 21:31:54 +0000

    12 March Update

--- SAMapp/routes.py
+++ SAMapp/routes.py
@@ -1,39 +1,34 @@
-from flask import render_template, url_for, flash, redirect
-from SAMapp import app
-#from SAMapp import qrgenerator 
+from flask import render_template, url_for, flash, redirect, request
+from SAMapp import app, db, bcrypt
 from SAMapp.forms import RegistrationForm, LoginForm
 from SAMapp.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
 	{
 		"author": "Niall Mclawrence",
-		"title": "Forum Post 1",
+		"title": "Forum Test 1",
 		"content": "First Post Content",
 		"date_posted": "Feb 16, 2021"
 	},
 	{
-		"author": "Thomas Philips",
-		"title": "Forum Post 2",
+		"author": "Thomas Phillips",
+		"title": "Forum Test 2",
 		"content": "Second Post Content",
 		"date_posted": "Feb 17, 2021"
 	},
 	{
 		"author": "Andrew Lovell",
-		"title": "Forum Post 3",
+		"title": "Forum Test 3",
 		"content": "Third Post Content",
-		"date_posted": "Feb 18, 2021"
+		"date_posted": "Feb 17, 2021"
 	}
 ]
 
-#Routing the pages
 @app.route("/")
 @app.route("/home")
 def home():
 	return render_template("home.html")
-
-@app.route("/qrgenerator")
-def qrgenerator():
-	return SAMapp.qrgenerator()
 
 @app.route("/logbook")
 def logbook():
@@ -49,19 +44,39 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}!', 'success')
-		return redirect(url_for('home'))
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')       #Hashes the password on submit
+		user = User(username=form.username.data, email=form.email.data, password=hashed_password) #Passes in the hashed password
+		db.session.add(user)
+		db.session.commit() #adds user to database
+		flash('Your account has been created! You can now log in', 'success')   #Shows message on creation
+		return redirect(url_for('login'))										#Sends them to login page
 	return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-			flash('You have been logged in!', 'success')
-			return redirect(url_for('home'))
+		user = User.query.filter_by(email=form.email.data).first()                  #Checks if email exists in our db
+		if user and bcrypt.check_password_hash(user.password, form.password.data):  #Same for password
+			login_user(user, remember=form.remember.data)
+			next_page = request.args.get('next')							
+			return  redirect(next_page) if  next_page else redirect(url_for('home'))
 		else:
-			flash('Login Unsuccessful. Please check username and password', 'danger')
-	return render_template('login.html', title='Login', form=form)
+			flash('Login Unsuccessful. Please check email and password', 'danger')	#if failed will show this message
+	return render_template('login.html', title='Login', form=form)	
+
+@app.route("/logout")
+def logout():
+	logout_user()	
+	return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+	return render_template('account.html', title='Account')
